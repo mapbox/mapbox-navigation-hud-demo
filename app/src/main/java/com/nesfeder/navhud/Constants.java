61561b60f95efbb59bb387463c3784d28a939a11
danesfeder <dan.nesfeder@mapbox.com>
2017-07-12 11:57:42 -0400

    Adding NavHUD project

---
+++ app/src/main/java/com/nesfeder/navhud/Constants.java
@@ -0,0 +1,27 @@
+package com.nesfeder.navhud;
+
+public class Constants {
+
+  public static final double MPH_DOUBLE = 2.2369;
+  public static final String MAPBOX_ACCESS_TOKEN = "";
+  public static final String PLACE_LOCATION_EXTRA = "PLACE_LOCATION_EXTRA";
+
+  // Step Maneuver Modifiers
+  static final String STEP_MANEUVER_MODIFIER_UTURN = "uturn";
+  static final String STEP_MANEUVER_MODIFIER_SHARP_RIGHT = "sharp right";
+  static final String STEP_MANEUVER_MODIFIER_RIGHT = "right";
+  static final String STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT = "slight right";
+  static final String STEP_MANEUVER_MODIFIER_STRAIGHT = "straight";
+  static final String STEP_MANEUVER_MODIFIER_SLIGHT_LEFT = "slight left";
+  static final String STEP_MANEUVER_MODIFIER_LEFT = "left";
+  static final String STEP_MANEUVER_MODIFIER_SHARP_LEFT = "sharp left";
+
+  public static final String DAYS = " days ";
+  public static final String HOUR = " hr ";
+  public static final String MINUTE = " min ";
+  public static final String SECONDS = " seconds";
+  public static final String MILES = " mi";
+  public static final double METER_MULTIPLIER = .00062137;
+  public static final String DURATION_ILLEGAL_ARGUMENT = "Duration must be greater than zero.";
+  public static final int STRING_BUILDER_CAPACITY = 64;
+}
---
+++ app/src/main/java/com/nesfeder/navhud/geocode/GeocodeActivity.java
@@ -0,0 +1,94 @@
+package com.nesfeder.navhud.geocode;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.nesfeder.navhud.R;
+import com.nesfeder.navhud.display.DisplayActivity;
+
+import static com.nesfeder.navhud.Constants.PLACE_LOCATION_EXTRA;
+
+public class GeocodeActivity extends AppCompatActivity {
+
+    private static final String TAG = GeocodeActivity.class.getSimpleName();
+    private static final int REQUEST_LOCATION_PERMISSION = 2;
+    private static final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geocode);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchSearchAutocomplete();
+            }
+        });
+
+        requestLocationPermission();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Toast.makeText(this, "Place picked: " + place.getName(), Toast.LENGTH_SHORT).show();
+                launchDisplay(place);
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                Log.i(TAG, status.getStatusMessage());
+            }
+        }
+    }
+
+    private void requestLocationPermission() {
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+          != PackageManager.PERMISSION_GRANTED) ||
+          (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED)) {
+
+            ActivityCompat.requestPermissions(this,
+              new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION},
+              REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    private void launchSearchAutocomplete() {
+        try {
+            Intent intent =
+                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                            .build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private void launchDisplay(Place place) {
+        Intent displayActivity = new Intent(this, DisplayActivity.class);
+        Location placeLocation = new Location(TAG);
+        placeLocation.setLatitude(place.getLatLng().latitude);
+        placeLocation.setLongitude(place.getLatLng().longitude);
+        displayActivity.putExtra(PLACE_LOCATION_EXTRA, placeLocation);
+        startActivity(displayActivity);
+    }
+}
